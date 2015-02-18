294425743536a162b14d591d887f42cea6c5df32
Farkas Péter <farkas.peter1995@gmail.com>
2015-02-18 22:03:28 +0100

    A metódus elfogad egy számot

--- src/main/java/hu/wolfmanfp/stringcalculator/Main.java
+++ src/main/java/hu/wolfmanfp/stringcalculator/Main.java
@@ -5,7 +5,7 @@ public class Main {
 
     public static void main(String[] args) {
         StringCalculator calc = new StringCalculator();
-        int test = calc.add("");
+        int test = calc.add("2");
         System.out.println(test);
     }
 
