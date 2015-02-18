66514d6ac832d08af8619c9f7edeac845500b94c
Farkas Péter <farkas.peter1995@gmail.com>
2015-02-18 21:59:38 +0100

    A metódus üres stringet elfogad

---
+++ src/main/java/hu/wolfmanfp/stringcalculator/Main.java
@@ -0,0 +1,12 @@
+package hu.wolfmanfp.stringcalculator;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        StringCalculator calc = new StringCalculator();
+        int test = calc.add("");
+        System.out.println(test);
+    }
+
+}
--- src/main/java/hu/wolfmanfp/stringcalculator/StringCalculator.java
+++ src/main/java/hu/wolfmanfp/stringcalculator/StringCalculator.java
@@ -1,5 +1,8 @@
 package hu.wolfmanfp.stringcalculator;
 
 public class StringCalculator {
-
+    int add(String numbers) {
+        if(numbers.isEmpty()) return 0;
+        else return Integer.parseInt(numbers);
+    }
 }
