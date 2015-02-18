88772b04a6661588189957799b1f14e66a6a1915
Farkas Péter <farkas.peter1995@gmail.com>
2015-02-19 00:00:01 +0100

    A metódus elfogad kettőnél több számot is

--- src/main/java/hu/wolfmanfp/stringcalculator/Main.java
+++ src/main/java/hu/wolfmanfp/stringcalculator/Main.java
@@ -5,7 +5,7 @@ public class Main {
 
     public static void main(String[] args) {
         StringCalculator calc = new StringCalculator();
-        int test = calc.add("10,4");
+        int test = calc.add("10,4,5,12,4");
         System.out.println(test);
     }
 
