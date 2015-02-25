3be4163b80d52ee617f69a8ecb4a3006a511e5f4
Farkas Péter <farkas.peter1995@gmail.com>
2015-02-26 00:05:29 +0100

    Fizz

--- src/main/java/com/wcs/fizzbuzz/FizzBuzzer.java
+++ src/main/java/com/wcs/fizzbuzz/FizzBuzzer.java
@@ -3,6 +3,7 @@ package com.wcs.fizzbuzz;
 public class FizzBuzzer {
     
     public String execute(int number) {
+        if (number%3==0) return "fizz";
         return Integer.toString(number);
     }
     
