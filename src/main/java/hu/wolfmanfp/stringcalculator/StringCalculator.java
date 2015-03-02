abb7933156b79702e32eab8fc66ef5f891b5a5c2
Farkas Péter <farkas.peter1995@gmail.com>
2015-03-03 00:36:09 +0100

    A metódus nem engedi meg a negatív számokat

--- src/main/java/hu/wolfmanfp/stringcalculator/StringCalculator.java
+++ src/main/java/hu/wolfmanfp/stringcalculator/StringCalculator.java
@@ -1,20 +1,31 @@
 package hu.wolfmanfp.stringcalculator;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class StringCalculator {
     public String delimiter;
 
     public StringCalculator() {
     }
     
-    public int add(String numbers){
+    public int add(String numbers) throws Exception{
         int sum = 0;
+        List<Integer> negativeList = new ArrayList<>();
         
         if (numbers.isEmpty()) return 0;
         String[] numberArray = numbers.split(delimiter);
         
-        for (String num : numberArray) {
-            sum += Integer.valueOf(num);
+        for (String numStr : numberArray) {
+            int num = Integer.valueOf(numStr);
+            
+            sum += num;
+            if (num<0) negativeList.add(num);
         }
+        
+        if (!negativeList.isEmpty()) throw new Exception("Negatives are not allowed: "+
+                Arrays.toString(negativeList.toArray()));
         
         return sum;
     }
--- src/test/java/hu/wolfmanfp/stringcalculator/StringCalcTest.java
+++ src/test/java/hu/wolfmanfp/stringcalculator/StringCalcTest.java
@@ -7,7 +7,9 @@ package hu.wolfmanfp.stringcalculator;
 
 import static org.junit.Assert.assertEquals;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 /**
  *
@@ -15,6 +17,9 @@ import org.junit.Test;
  */
 public class StringCalcTest {
     StringCalculator calculator;
+    
+    @Rule
+    public ExpectedException expected = ExpectedException.none();
     
     @Before
     public void setUp() {
@@ -26,35 +31,42 @@ public class StringCalcTest {
     }
     
     @Test
-    public void emptyString() {
+    public void emptyString() throws Exception {
         assertEquals(0, calculator.add(""));
     }
     
     @Test
-    public void oneNumber() {
-        assertEquals(1, calculator.add("1"));
-        assertEquals(2, calculator.add("2"));
+    public void oneNumber() throws Exception {
+            assertEquals(1, calculator.add("1"));
+            assertEquals(2, calculator.add("2"));
     }
     
     @Test
-    public void twoNumbers() {
+    public void twoNumbers() throws Exception {
         assertEquals(4, calculator.add("1,3"));
         assertEquals(14, calculator.add("10,4"));
     }
     
     @Test
-    public void multipleNumbers() {
+    public void multipleNumbers() throws Exception {
         assertEquals(10, calculator.add("1,3,1,5"));
         assertEquals(35, calculator.add("10,4,5,12,4"));
     }
     
     @Test
-    public void customDelimiters() {
+    public void customDelimiters() throws Exception {
         calculator.setDelimiter(";");
         assertEquals(3, calculator.add("1;2"));
         calculator.setDelimiter("\n");
         assertEquals(8, calculator.add("3\n5"));
         calculator.setDelimiter("\t");
         assertEquals(6, calculator.add("4\t2"));
+    }
+    
+    @Test
+    public void negativeNumbers() throws Exception {
+        expected.expect(Exception.class);
+        expected.expectMessage("Negatives are not allowed: [-3, -5]");
+        calculator.add("1,-3,2,-5");
     }
 }
