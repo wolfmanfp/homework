6a7736b760958a72e7c001b2f63d91be9063b138
Farkas Péter <farkas.peter1995@gmail.com>
2015-03-02 23:53:12 +0100

    A metódus elfogad két számot

--- src/main/java/hu/wolfmanfp/stringcalculator/StringCalculator.java
+++ src/main/java/hu/wolfmanfp/stringcalculator/StringCalculator.java
@@ -2,9 +2,11 @@ package hu.wolfmanfp.stringcalculator;
 
 public class StringCalculator {
     
-    public int add(String numbers){
+    public int add(String numbers){   
         if (numbers.isEmpty()) return 0;
-        return Integer.valueOf(numbers);
+        String[] numberArray = numbers.split(",");
+        if (numberArray.length==1) return Integer.valueOf(numberArray[0]);
+        return Integer.valueOf(numberArray[0])+Integer.valueOf(numberArray[1]);
     }
     
 }
--- src/test/java/hu/wolfmanfp/stringcalculator/StringCalcTest.java
+++ src/test/java/hu/wolfmanfp/stringcalculator/StringCalcTest.java
@@ -26,12 +26,18 @@ public class StringCalcTest {
     
     @Test
     public void emptyString() {
-        assertEquals(0,calculator.add(""));
+        assertEquals(0, calculator.add(""));
     }
     
     @Test
     public void oneNumber() {
-        assertEquals(1,calculator.add("1"));
-        assertEquals(2,calculator.add("2"));
+        assertEquals(1, calculator.add("1"));
+        assertEquals(2, calculator.add("2"));
+    }
+    
+    @Test
+    public void twoNumbers() {
+        assertEquals(4, calculator.add("1,3"));
+        assertEquals(14, calculator.add("10,4"));
     }
 }
