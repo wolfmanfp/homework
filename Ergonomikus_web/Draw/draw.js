9ebe186162c8754fec4667eade110321be38f806
Farkas Péter <farkas.peter1995@gmail.com>
2016-03-03 08:54:12 +0100

    Saving pictures to localStorage is now possible

--- Ergonomikus_web/Draw/draw.js
+++ Ergonomikus_web/Draw/draw.js
@@ -36,7 +36,51 @@ function addColor(colorName) {
     );
 }
 
+function loadButtons() {
+    var $buttons = $('#buttons');
+    $buttons.append($('<div>')
+        .addClass('save')
+        .text('Save')
+        .click(
+            function() {
+                savePicture();
+            }
+        ));
+    $buttons.append($('<div>')
+        .addClass('load')
+        .text('Load')
+        .click(
+            function() {
+                loadPicture();
+            }
+        ));
+}
+
+function savePicture(picture){
+    var picture = [];
+    $('#tableholder tr').each(function() {
+        var td = [];
+        $(this).children().each(function(){
+            var className = $(this).attr('class');
+            if (className == 'red') td.push("r");
+            else if (className == 'green') td.push("g");
+            else if (className == 'blue') td.push("b");
+            else if (className == 'black') td.push("k");
+            else td.push('');
+        });
+        picture.push(td);
+    });
+    var pictureJSON = JSON.stringify(picture);
+    console.log(pictureJSON);
+    localStorage.setItem('picture', picture);
+}
+
+function loadPicture() {
+
+}
+
 window.onload = function(){
     createTable();
     changeColor();
+    loadButtons();
 }
