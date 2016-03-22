14dcc2c5b1879b650e2f7011eef98d4babf3501e
Farkas Péter <farkas.peter1995@gmail.com>
2016-03-22 03:59:02 +0100

    "New Game" function, design

--- numgame.js
+++ numgame.js
@@ -1,9 +1,13 @@
 function startGame() {
-    $(".start").click(function(){
+    $(".get_token").click(function(){
+        $("#form").show();
         $("#intro").hide();
+        $("#status").text("");
+        $("div.get_token").hide();
         $("#game").show();
-        $.get("http://api.gamer365.hu/numgame/register", function(data){
-            if(data.status=="ok") {
+        $("#guess").select();
+        $.get("http://api.gamer365.hu/numgame/register", function (data) {
+            if (data.status == "ok") {
                 var token = data.token;
                 guess(token);
             }
@@ -16,6 +20,7 @@ function guess(token) {
         $guess = $("#guess");
         var number = $guess.val();
         $guess.val("");
+        var $status = $("#status");
         if ($.isNumeric(number) && number>=1 && number<=100) {
             $.post("http://api.gamer365.hu/numgame/guess",
                 {
@@ -23,7 +28,6 @@ function guess(token) {
                     value: number
                 },
                 function(response){
-                    var $status = $("#status");
                     switch (response.status) {
                         case "ok":
                             switch(response.answer) {
@@ -35,17 +39,19 @@ function guess(token) {
                                     break;
                                 case "win":
                                     var guesses = response.guesses;
+                                    $("#game").hide();
                                     $status.text("You win! Number of guesses: "+guesses);
+                                    $("div.get_token").show();
                                     break;
                             }
                             break;
                         case "invalid token":
-                            alert("Invalid token!");
+                            $status.text("Invalid token!");
                             break;
                     }
                 });
         } else {
-            alert("Invalid number!");
+            $status.text("Invalid number!");
         }
     });
 }
