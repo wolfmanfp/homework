aef392ad72d4994093bd83e37da3571f22a7461e
Farkas Péter <farkas.peter1995@gmail.com>
2016-05-17 19:28:58 +0200

    Eladó kliens UI, User osztály

---
+++ EladoKliens/User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace EladoKliens
+{
+    [DataContract]
+    public class User
+    {
+        [DataMember]
+        public int ID { get; set; }
+
+        [DataMember]
+        public string UserName { get; set; }
+
+        [DataMember]
+        public string Password { get; set; }
+
+        [DataMember]
+        public string Location { get; set; }
+
+        public User()
+        {
+        }
+    }
+}
