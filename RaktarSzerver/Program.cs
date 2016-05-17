dd1442da8eba72df0a7f5686a00fbe97b6cb2573
László Zoltán <pacee08@gmail.com>
2016-05-17 19:25:20 +0200

    Szerver alap

--- RaktarSzerver/Program.cs
+++ RaktarSzerver/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
 
 namespace RaktarSzerver
 {
@@ -10,7 +11,22 @@ namespace RaktarSzerver
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("valamiu");
+            try
+            {
+                Console.WriteLine("Starting server...");
+                Console.WriteLine("Connecting SQL server...");
+                DBConnection dbConnection = DBConnection.Instance;
+                SqlConnection connection = dbConnection.GetConnection();
+                connection.Open();
+                Console.WriteLine("Connecting SQL server... Success.");
+                Console.Read();
+
+            }
+            catch(Exception exc)
+            {
+                Console.WriteLine(exc.Message);
+                Console.Read();
+            }
         }
     }
 }
