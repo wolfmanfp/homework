59ad921e3df209d2cacc44d0955094fa97403c77
Harmat Tamás <harmath.tamas93@gmail.com>
2016-05-19 13:36:14 +0200

    frissites

---
+++ RaktarSzerver/SQLTasks/Authentication.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data.Sql;
+using System.Data;
+
+namespace RaktarSzerver
+{
+    public class Authentication
+    {
+        private SqlConnection connection;
+        private string commandText =
+            "SELECT Password FROM Users WHERE Name = @Name";
+        private SqlCommand command;
+        private static Authentication instance;
+        public static Authentication Instance{
+            get{
+                if (instance == null){
+                    instance = new Authentication();
+                }return instance;
+            }
+        }
+
+        private Authentication() { }
+
+        public bool Authenticate(string UserName, string Password)
+        {
+            this.connection = DBConnection.Instance.GetConnection();
+            this.command = new SqlCommand();
+            this.command.CommandText = commandText;
+            this.command.Connection = connection;
+            this.command.Parameters.Add("@Name", SqlDbType.NVarChar).Value = UserName;
+            try
+            {
+                connection.Open();
+                string passw = (string)command.ExecuteScalar();
+                connection.Close();
+
+                if (passw == Password)
+                {
+                    return true;
+                }else {
+                    return false;
+                }
+            }
+            catch (Exception)
+            {
+                connection.Close();
+                throw;
+            }
+        }
+    }
+}
--- RaktarosKliens/MainWindow.xaml.cs
+++ RaktarosKliens/MainWindow.xaml.cs
@@ -23,6 +23,57 @@ namespace RaktarosKliens
         public MainWindow()
         {
             InitializeComponent();
+            this.DataContext = this;
+        }
+
+        private void btnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            if (txtUserName.Text == "" || txtPassWord.Text == "") {
+                MessageBox.Show("Minden mezőt ki kell tölteni!");
+            }
+            else
+            {
+                sp1.Visibility = Visibility.Visible;
+                sp2.Visibility = Visibility.Visible;
+                MessageBox.Show(txtUserName.Text + " " + txtPassWord.Text);
+            }
+            
+        }
+
+        private void btnGetProducts_Click(object sender, RoutedEventArgs e)
+        {
+            if (txtProductNameLeft.Text == "" || txtProductPiecesLeft.Text == "" || txtLocationLeft.Text == "")
+            {
+                MessageBox.Show("Minden mezőt ki kell tölteni!");
+            }
+            else
+            {
+                MessageBox.Show(txtProductNameLeft.Text + " " + txtProductPiecesLeft.Text + " " + txtLocationLeft.Text);
+            }
+        }
+
+        private void btnAddProducts_Click(object sender, RoutedEventArgs e)
+        {
+            if (txtProductNameLeft.Text == "" || txtProductPiecesLeft.Text == "" || txtLocationLeft.Text == "")
+            {
+                MessageBox.Show("Minden mezőt ki kell tölteni!");
+            }
+            else
+            {
+                MessageBox.Show(txtProductNameLeft.Text + " " + txtProductPiecesLeft.Text + " " + txtLocationLeft.Text);
+            }
+        }
+
+        private void btnTransferProducts_Click(object sender, RoutedEventArgs e)
+        {
+            if (txtProductNameRight.Text == "" || txtNewLocationRight.Text == "" || txtOldLocationRight.Text == "")
+            {
+                MessageBox.Show("Minden mezőt ki kell tölteni!");
+            }
+            else
+            {
+                MessageBox.Show(txtProductNameRight.Text + " " + txtNewLocationRight.Text + " " + txtOldLocationRight.Text);
+            }
         }
     }
 }
