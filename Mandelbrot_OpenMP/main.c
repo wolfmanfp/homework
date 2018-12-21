0480ee8c52f004d02ad72ff5abbcb1f6f1efaa73
Farkas Péter <farkas.peter1995@gmail.com>
2018-12-21 14:22:27 +0100

    Párhuzamosított program

--- Mandelbrot_OpenMP/main.c
+++ Mandelbrot_OpenMP/main.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
-#include <time.h>
+#include <omp.h>
 
 #define SIZE 50					// A halmaz mérete
-#define MAX_ITERATION 100		// Az iterációk száma
+#define MAX_ITERATION 10000		// Az iterációk száma
 
 char mandelbrot_set[SIZE][SIZE];
+int num_threads;
 
 /**
 A Mandelbrot-halmaz kiszámítása "pixelenként".
@@ -14,50 +15,58 @@ void calculate_mandelbrot_set()
 	// A komplex síkot a megadott méret alapján felosztjuk, és ezeket vizsgáljuk
 	// Az X és Y tengelyt felosztjuk a megadott méret alapján
 	// iterateX, iterateY: mennyi távolság van két pixel közt
-	double iterateX = 3.5 / SIZE, iterateY = 2.0 / SIZE;
-	int setY = 0;
-
 	// cx, cy: komplex sík valós és képzetes tengelyének számértékei
-	for (double cy = -1; cy < 1; cy += iterateY) {
-		int setX = 0;
+	double iterateX = 3.5 / SIZE, iterateY = 2.0 / SIZE, cy = -1;
+	int setX, setY;
 
-		for (double cx = -2.5; cx < 1; cx += iterateX) {
-			double x = 0.0, y = 0.0, xtemp = 0.0;
-			int iteration = 0;
+#pragma omp parallel private(setX, setY)
+	{
+		num_threads = omp_get_num_threads();
+		
+#pragma omp for ordered
+		for (setY = 0; setY < SIZE; setY++) {
+			double cx = -2.5;
 
-			// Iteráció, amíg az abszolútérték el nem éri a 2^2-et vagy a határt.
-			while (x * x + y * y <= 4.0 && iteration < MAX_ITERATION)
-			{
-				xtemp = x * x - y * y + cx;
-				y = 2 * x * y + cy;
-				x = xtemp;
-				iteration++;
+#pragma omp ordered
+			for (setX = 0; setX < SIZE; setX++) {
+				double x = 0.0, y = 0.0, xtemp = 0.0;
+				int iteration = 0;
+
+				// Iteráció, amíg az abszolútérték el nem éri a 2^2-et vagy a határt.
+				while (x * x + y * y <= 4.0 && iteration < MAX_ITERATION)
+				{
+					xtemp = x * x - y * y + cx;
+					y = 2 * x * y + cy;
+					x = xtemp;
+					iteration++;
+				}
+
+				// Ha elérte a határt, a halmaz adott elemének beállítunk egy karaktert,
+				// ellenkezõ esetben szóközt adunk meg.
+				if (iteration == MAX_ITERATION) {
+					mandelbrot_set[setX][setY] = '*';
+				}
+				else {
+					mandelbrot_set[setX][setY] = ' ';
+				}
+
+				// következõ pont
+				cx += iterateX;
 			}
 
-			// Ha elérte a határt, a halmaz adott elemének beállítunk egy karaktert,
-			// ellenkezõ esetben szóközt adunk meg.
-			if (iteration == MAX_ITERATION) {
-				mandelbrot_set[setX][setY] = '*';
-			}
-			else {
-				mandelbrot_set[setX][setY] = ' ';
-			}
-			
-			// következõ oszlop
-			setX++;
+			cy += iterateY;
 		}
-
-		// következõ sor
-		setY++;
 	}
 }
 
 /**
 A Mandelbrot-halmaz képének kiíratása.
 */
-void output_set() {
-	for (int x = 0; x < SIZE; x++) {
-		for (int y = 0; y < SIZE; y++) {
+void output_set() 
+{
+	int x, y;
+	for (x = 0; x < SIZE; x++) {
+		for (y = 0; y < SIZE; y++) {
 			// A két tengelyt megcserélve íratjuk ki a halmaz képét.
 			printf("%c", mandelbrot_set[y][x]);
 		}
@@ -67,15 +76,15 @@ void output_set() {
 
 int main() 
 {
-	clock_t start, end;
-	double diff;
+	double start, end, diff;
 
-	start = clock();
+	start = omp_get_wtime();
 	calculate_mandelbrot_set();
-	end = clock();
-	diff = ((double) end - start) / CLOCKS_PER_SEC;
+	end = omp_get_wtime();
+	diff = end - start;
 
 	output_set();
 	printf("A Mandelbrot-halmaz kiszamitasa %.5lf mp-et vett igenybe.\n", diff);
+	printf("A program %d szalon futott.", num_threads);
 	return 0;
 }
