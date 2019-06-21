12b9ecbdd7e5197f6e4b54326e9b5f71cf72b7a6
Farkas Péter <farkas.peter1995@gmail.com>
2019-06-21 21:47:39 +0200

    MPI homework: add parallel code

--- parallel/mpi/factorial.cpp
+++ parallel/mpi/factorial.cpp
@@ -1,22 +1,47 @@
 #include <iostream>
+#include <openmpi/mpi.h>
 using namespace std;
 
+const int MASTER = 0;
 const int NUMBER = 20;
 
 int main (int argc, char **argv)
 {
-    unsigned long long szorzat = 1;
+    unsigned long long resz_szorzat = 1, szorzat = 1;
+    int id, nproc;
     time_t time_start, time_end;
 
     srand(time(NULL));
-    time_start = clock();
-    for (int i = 1; i <= NUMBER; i++)
-    {
-        szorzat *= i;
-    }
-    time_end = clock();
+    MPI_Init(&argc, &argv);
+    MPI_Status status;
+    MPI_Comm_rank(MPI_COMM_WORLD, &id);
+    MPI_Comm_size(MPI_COMM_WORLD, &nproc);
 
-    cout << "Eltelt ido: "
-         << difftime(time_end, time_start) / CLOCKS_PER_SEC << " mp" << endl;
-    cout << NUMBER << " faktorialisa: " << szorzat << "." << endl;
+    if (id == MASTER) {
+        time_start = clock();
+    }
+
+    for (int i = 1 + id; i <= NUMBER; i += nproc) // loop splitting
+    {
+        szorzat *= i; // node részszorzata
+    }
+
+    if (id == MASTER) { // a master összegyűjti a részszorzatokat
+        for (int j = 1; j < nproc; j++)
+        {
+            MPI_Recv(&resz_szorzat, 1, MPI_INT, MPI_ANY_SOURCE, 1, MPI_COMM_WORLD, &status);
+            szorzat *= resz_szorzat;
+        }
+        time_end = clock();
+    } else { // slave-ek elküldik a részszorzatot
+        MPI_Send(&szorzat, 1, MPI_INT, 0, 1, MPI_COMM_WORLD);
+    }
+    
+    if (id == MASTER) 
+    {
+        cout << "Eltelt ido: "
+             << difftime(time_end, time_start) / CLOCKS_PER_SEC << " mp" << endl;
+        cout << NUMBER << " faktorialisa: " << szorzat << "." << endl;
+    }
+    MPI_Finalize();
 }
