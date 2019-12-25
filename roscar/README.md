df2ef5dc14d6af8766636f946ec40f2345c32925
Farkas Péter <farkas.peter1995@gmail.com>
2019-12-25 18:00:01 +0100

    Add ROS project

---
+++ roscar/README.md
@@ -0,0 +1,9 @@
+# roscar
+
+Controlling a simulated robot using WASD keys.
+
+This project is based on:
+* [MooreRobots' beginner tutorial](http://moorerobots.com/blog/post/1)
+* [Turtle keyboard teleoperation code](https://github.com/ros/ros_tutorials/blob/kinetic-devel/turtlesim/tutorials/teleop_turtle_key.cpp) from the official [ROS tutorial](http://wiki.ros.org/ROS/Tutorials/UnderstandingTopics)
+
+I also used the [pysdf](https://github.com/andreasBihlmaier/pysdf) library to convert my Gazebo model into URDF format.
---
+++ roscar/src/roscar_control/src/car_teleop_key.cpp
@@ -0,0 +1,110 @@
+#include <signal.h>
+#include <stdio.h>
+#include <termios.h>
+#include <ros/ros.h>
+#include <geometry_msgs/Twist.h>
+
+#define KEYCODE_UP 'w'
+#define KEYCODE_LEFT 'a'
+#define KEYCODE_DOWN 's'
+#define KEYCODE_RIGHT 'd' 
+
+class Car
+{
+  public:
+    Car();
+    void keyLoop();
+
+  private:
+    ros::NodeHandle nh;
+    double linear, angular, lScale, aScale;
+    ros::Publisher twistPub;
+};
+
+Car::Car() : linear(0), angular(0), lScale(0.1), aScale(0.1)
+{
+  nh.param("scale_angular", aScale, aScale);
+  nh.param("scale_linear", lScale, lScale);
+
+  twistPub = nh.advertise<geometry_msgs::Twist>("/cmd_vel", 1);
+}
+
+int kfd = 0;
+struct termios before, after;
+
+void quit(int sig)
+{
+  (void)sig;
+  tcsetattr(kfd, TCSANOW, &before);
+  ros::shutdown();
+  exit(0);
+}
+
+int main(int argc, char **argv)
+{
+  ros::init(argc, argv, "car_teleop");
+  Car car;
+  signal(SIGINT, quit);
+
+  car.keyLoop();
+
+  return 0;
+}
+
+void Car::keyLoop()
+{
+  char c;
+  bool dirty = false;
+
+  // get the console in raw mode
+  tcgetattr(kfd, &before);
+  memcpy(&after, &before, sizeof(struct termios));
+  after.c_lflag &= ~(ICANON | ECHO);
+  tcsetattr(kfd, TCSANOW, &after);
+
+  puts("Reading from keyboard...");
+  puts("Use WASD keys to move the car.");
+
+  while (ros::ok())
+  {
+    c = getchar();
+
+    linear = angular = 0;
+    ROS_DEBUG("value: 0x%02X\n", c);
+
+    switch (c)
+    {
+      case KEYCODE_LEFT:
+        ROS_DEBUG("LEFT");
+        angular = 1.0;
+        dirty = true;
+        break;
+      case KEYCODE_RIGHT:
+        ROS_DEBUG("RIGHT");
+        angular = -1.0;
+        dirty = true;
+        break;
+      case KEYCODE_UP:
+        ROS_DEBUG("UP");
+        linear = 1.0;
+        dirty = true;
+        break;
+      case KEYCODE_DOWN:
+        ROS_DEBUG("DOWN");
+        linear = -1.0;
+        dirty = true;
+        break;
+    }
+
+    geometry_msgs::Twist twist;
+    twist.angular.z = aScale * angular;
+    twist.linear.x = lScale * linear;
+    if (dirty == true)
+    {
+      twistPub.publish(twist);
+      dirty = false;
+    }
+  }
+
+  return;
+}
